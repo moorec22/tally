bb11220b9e4878feb839b1f3432d4374c48921f9
Connor <moorec104@gmail.com>
2026-05-30 20:55:54 -0700

    task: set up rspec

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Tally
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |generators|
+      generators.test_framework :rspec
+    end
   end
 end
