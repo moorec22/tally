de58a333d29d8f941b71d92c673d8ab63c80561a
Connor <moorec104@gmail.com>
2026-05-31 22:38:35 -0700

    task: remove individual creation endpoint for inventory snapshot

--- app/controllers/inventory_snapshots_controller.rb
+++ app/controllers/inventory_snapshots_controller.rb
@@ -1,17 +1,4 @@
 class InventorySnapshotsController < ApplicationController
-  def create
-    item = Item.find(params[:item_id])
-    inventory_snapshot = item.inventory_snapshots.build(inventory_snapshot_params)
-
-    if inventory_snapshot.save
-      inventory_snapshot_presenter = InventorySnapshotPresenter.new(inventory_snapshot)
-
-      render json: inventory_snapshot_presenter.as_json, status: :created
-    else
-      render json: { errors: inventory_snapshot.errors.to_hash }, status: :unprocessable_content
-    end
-  end
-
   def bulk
     snapshot_attributes = inventory_snapshots_params
     item_ids = snapshot_attributes.map { |snapshot_params| snapshot_params[:item_id].to_i }
@@ -43,10 +30,6 @@ class InventorySnapshotsController < ApplicationController
   end
 
   private
-
-  def inventory_snapshot_params
-    params.require(:inventory_snapshot).permit(:value, :note)
-  end
 
   def inventory_snapshots_params
     params.require(:inventory_snapshots).map do |snapshot_params|
